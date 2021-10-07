888fa42219f1fc5a9dbb85cbb2e8701e33d12f2c
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2021-10-07 11:06:39 +0200

    dummy secret key until it is read from .env

--- app/settings.py
+++ app/settings.py
@@ -28,7 +28,8 @@ COMPANY_NAME = "Cloud Outdated"
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ENV.str("DJANGO_SECRET_KEY")
+# SECRET_KEY = ENV.str("DJANGO_SECRET_KEY")
+SECRET_KEY = "foo243249832749832798fsfkjsdhf-3333"
 DUMMY_VALUE = ENV.str("DUMMY_VALUE")
 
 # SECURITY WARNING: don't run with debug turned on in production!
