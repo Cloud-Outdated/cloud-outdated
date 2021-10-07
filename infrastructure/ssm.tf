bc6556ef8ec277e60643bf2199c2cad86e939d74
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2021-10-07 12:10:11 +0200

    add parameter store KMS key

--- infrastructure/ssm.tf
+++ infrastructure/ssm.tf
@@ -1,3 +1,14 @@
+resource "aws_kms_key" "parameter_store" {
+  description             = "Parameter store KMS master key"
+  deletion_window_in_days = 10
+  enable_key_rotation     = true
+}
+
+resource "aws_kms_alias" "parameter_store_alias" {
+  name          = "alias/parameter_store_key"
+  target_key_id = "${aws_kms_key.parameter_store.id}"
+}
+
 resource "random_string" "app-secret-key" {
     length  = 50
     upper   = true
