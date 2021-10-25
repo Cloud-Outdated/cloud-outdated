a78b0d6971f396c92976418d269ebec30cd7414e
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2021-10-25 11:02:51 +0200

    set vpc subnet and security group for rds access

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -21,3 +21,17 @@ output "rds_password" {
   value       = module.rds.db_instance_password
   sensitive   = true
 }
+
+output "rds_subnet_ids" {
+  description = "RDS VPC subnet ids"
+  value = module.vpc.database_subnets
+  sensitive = false
+}
+
+output "rds_vpc_security_group_ids" {
+  description = "RDS VPC security group ids"
+  value = [module.security_group.security_group_id]
+  sensitive = false
+}
+
+  
