0b25d525f84a8074dbbb26682d516108e5afee71
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2021-10-25 10:39:56 +0200

    test event printout cleanup

--- core/tasks.py
+++ core/tasks.py
@@ -5,5 +5,4 @@ from django.conf import settings
 def task_one():
     # here we can define periodic tasks
     # and call them in zappa_settings.json
-    print(f"yes no maybe - {datetime.datetime.now().isoformat()}")
-    print(f"settings: {settings.DEFAULT_AUTO_FIELD}")
+    print(f"test event printout - {datetime.datetime.now().isoformat()}")
