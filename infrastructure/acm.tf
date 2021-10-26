b1e96786bcc5501630e0d485cc9fbc214fb35efa
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2021-10-27 01:41:00 +0200

    set certificate in us-east-1

--- infrastructure/acm.tf
+++ infrastructure/acm.tf
@@ -1,6 +1,7 @@
 resource "aws_acm_certificate" "main" {
   domain_name = var.environment != "prod" ? "${var.environment}.${local.project}.com" : "${local.project}.com"
   validation_method = "DNS"
+  provider = "aws.virginia"
 
   lifecycle {
     create_before_destroy = true
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -28,3 +28,8 @@ provider "aws" {
     }
   }
 }
+
+provider "aws" {
+  alias = "virginia"
+  region = "us-east-1"
+}
