996fd456149777cd34d82be2699422adbd093e65
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2021-11-02 21:11:21 +0100

    add egress rule for postgres

--- infrastructure/security_group.tf
+++ infrastructure/security_group.tf
@@ -6,17 +6,14 @@ module "security_group" {
   description = "${local.project} ${var.environment} security group"
   vpc_id      = module.vpc.vpc_id
 
-  # TODO switch ingress to VPC only
   ingress_with_cidr_blocks = [
-    # vpc only ingress
-    # {
-    #   from_port   = 5432
-    #   to_port     = 5432
-    #   protocol    = "tcp"
-    #   description = "PostgreSQL access from within VPC"
-    #   cidr_blocks = module.vpc.vpc_cidr_block
-    # },
-    # public ingress
+    {
+      rule        = "postgresql-tcp"
+      cidr_blocks = "0.0.0.0/0"
+    },
+  ]
+
+  egress_with_cidr_blocks = [
     {
       rule        = "postgresql-tcp"
       cidr_blocks = "0.0.0.0/0"
