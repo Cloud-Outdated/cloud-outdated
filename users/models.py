0f4220e49ea6f5ab530ef8db31208abab519e2c1
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2021-11-03 08:40:02 +0100

    fix user creation with password

--- users/models.py
+++ users/models.py
@@ -17,7 +17,7 @@ class UserProfileManager(BaseUserManager):
             raise ValueError("User must have an email address")
 
         email = self.normalize_email(email)
-        user = self.model(email=email)
+        user = self.model(email=email, password=password)
 
         user.set_password(password)
         user.save(using=self._db)
