060bfee325c58879e40c8667a7abcb05afe3940f
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2021-11-03 08:58:14 +0100

    fix django admin for user

--- users/admin.py
+++ users/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.utils.translation import gettext, gettext_lazy as _
+
 from .models import UserProfile
 
 
 @admin.register(UserProfile)
-class UserProfileAdmin(admin.ModelAdmin):
+class UserProfileAdmin(DjangoUserAdmin):
+    add_fieldsets = (
+        (None, {"classes": ("wide",), "fields": ("email", "password1", "password2")}),
+    )
     list_display = [
         "id",
         "email",
@@ -11,11 +17,29 @@ class UserProfileAdmin(admin.ModelAdmin):
         "is_staff",
         "is_active",
     ]
-    search_fields = ["id", "email"]
+    search_fields = ["id", "email", "first_name", "last_name"]
     list_filter = [
         "is_staff",
         "is_superuser",
         "is_active",
         "created",
     ]
+    ordering = ["created"]
     date_hierarchy = "created"
+    fieldsets = (
+        (None, {"fields": ("password",)}),
+        (_("Personal info"), {"fields": ("first_name", "last_name", "email")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
