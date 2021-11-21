e416267c8eebd84789248d5170f15f2726870fbb
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2021-11-22 00:19:13 +0100

    add test for Service label default set

--- services/tests/test_base.py
+++ services/tests/test_base.py
@@ -1,6 +1,26 @@
 from django.test import SimpleTestCase
 
-from services.base import services
+from services.base import Service, services
+
+
+class ServiceLabelSetTestCase(SimpleTestCase):
+    def test_label_was_set(self):
+        dummy = Service(
+            platform=None,
+            name="foo",
+            name_alternatives=[],
+            label="bar",
+        )
+        assert dummy.label == "bar"
+
+    def test_label_was_not_set(self):
+        dummy = Service(
+            platform=None,
+            name="foo",
+            name_alternatives=[],
+            label=None,
+        )
+        assert dummy.label == "foo"
 
 
 class ServicesHaveUniqueNameTestCase(SimpleTestCase):
