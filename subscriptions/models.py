4ff3f5da8ae4dba6edc8fcf78eb1cd1fef962c3d
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2022-03-07 11:23:54 +0100

    docstring for user subscribe class method

--- subscriptions/models.py
+++ subscriptions/models.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.db import models
 
 from core.models import BaseModelMixin
-from services.base import service_choices
+from services.base import services, service_choices
 
 
 class Subscription(BaseModelMixin):
@@ -14,6 +14,18 @@ class Subscription(BaseModelMixin):
 
     @classmethod
     def subscribe_user_to_service(cls, user, service):
+        """Subscribe user to service if such service exists and user is not
+        already subcribed.
+
+        If user has an active subscription, do not fail, just return that
+        subscription instance
+
+        Args:
+            user (users.models.UserProfile): user instance
+            service (str): service key
+        """
+        assert service in services
+
         subscription, _ = cls.objects.get_or_create(
             user=user,
             service=service,
