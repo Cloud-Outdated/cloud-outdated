9cc55f3bfebdb69f96eaf5babae8612a0a626e03
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2022-03-07 22:03:18 +0100

    add service_is_public property to Version model

--- services/tests/test_models.py
+++ services/tests/test_models.py
@@ -1,7 +1,8 @@
+from unittest.mock import patch
 from grpc import services
 from services.tests.factories import VersionFactory
 from django.test import TestCase
-from services.base import services
+from services.base import Service, services, aws
 
 
 class VersionServiceLabelTestCase(TestCase):
@@ -14,3 +15,28 @@ class VersionServiceLabelTestCase(TestCase):
         version = VersionFactory(deprecated=None, service="foo_bar")
 
         assert version.service_label == "foo_bar"
+
+
+class VersionServiceIsPublicTestCase(TestCase):
+    def test_service_is_public(self):
+        version = VersionFactory(service=services["aws_aurora"].name)
+
+        assert version.service_is_public is True
+
+    def test_service_is_not_public(self):
+        non_public_aws_aurora = Service(
+            platform=aws,
+            name="aws_aurora",
+            name_alternatives=[],
+            public=False,
+        )
+
+        with patch.dict(services, {"aws_aurora": non_public_aws_aurora}):
+            version = VersionFactory(service=services["aws_aurora"].name)
+
+            assert version.service_is_public is False
+
+    def test_service_was_not_found(self):
+        version = VersionFactory(service="foo_bar")
+
+        assert version.service_is_public is False
