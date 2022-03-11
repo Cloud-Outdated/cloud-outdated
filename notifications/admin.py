dc21c3d9afeb332495d5df459c72fe4dc97cf3ef
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2022-03-11 23:06:06 +0100

    notification admin update and default model __str__ method

--- notifications/admin.py
+++ notifications/admin.py
@@ -3,13 +3,23 @@ from django.contrib import admin
 from notifications.models import Notification, NotificationItem
 
 
+class NotificationInlineAdmin(admin.TabularInline):
+    model = NotificationItem
+    extra = 0
+    show_change_link = True
+    can_delete = False
+    raw_id_fields = ["version"]
+
+
 @admin.register(Notification)
 class NotificationAdmin(admin.ModelAdmin):
-    list_display = ["id", "user", "sent"]
+    list_display = ["id", "user", "sent", "is_initial"]
     search_fields = ["id", "user__id", "user__email"]
-    list_filter = ["sent", "created"]
+    list_filter = ["sent", "created", "is_initial"]
     list_select_related = ["user"]
     date_hierarchy = "created"
+    inlines = [NotificationInlineAdmin]
+    raw_id_fields = ["user"]
 
 
 @admin.register(NotificationItem)
@@ -19,3 +29,5 @@ class NotificationItemAdmin(admin.ModelAdmin):
     list_filter = ["created"]
     list_select_related = ["notification", "version"]
     date_hierarchy = "created"
+    list_select_related = ["notification", "version"]
+    raw_id_fields = ["notification", "version"]
