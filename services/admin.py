cd52a13d26aa66c573581a176b9530f07af7a272
Mislav Cimperšak <mislav.cimpersak@gmail.com>
2022-03-29 19:29:04 +0200

    add platform to service versions admin

--- services/admin.py
+++ services/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 
 from services.models import Version
+from services.base import services
 
 
 @admin.register(Version)
 class VersionAdmin(admin.ModelAdmin):
-    list_display = ["id", "service", "version"]
+    list_display = ["id", "platform", "service", "version"]
     search_fields = ["id", "service", "version"]
     list_filter = ["service", "created"]
     date_hierarchy = "created"
+
+    def platform(self, obj):
+        service = services.get(obj.service)
+        if service:
+            return service.platform.name
+        else:
+            return "-"
